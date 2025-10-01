0ac548c40029bb2fd1e855d8af9d5f0dc321a789
Ca1eb334 <jfcaleb150@gmail.com>
2025-09-30 22:26:29 -0600

    ERRORES
    - Se corrigieron los procedimientos

--- src/procedimientos/proc_updatecategoria.sql
+++ src/procedimientos/proc_updatecategoria.sql
@@ -1,12 +1,13 @@
 CREATE OR REPLACE PROCEDURE update_categoria(
     or_category_name categorias.category_name%TYPE,
-    category_name categorias.category_name%TYPE
+    category_name categorias.category_name%TYPE,
+    purl categorias.url_cat%TYPE
 )
 IS
     opSql VARCHAR(255);
 BEGIN
-    opSql := 'UPDATE categorias SET category_name = :category_name WHERE category_name = :or_categoria_name';
-    EXECUTE IMMEDIATE opSql USING category_name, or_category_name;
+    opSql := 'UPDATE categorias SET category_name = :category_name, url_cat = :purl_cat WHERE category_name = :or_categoria_name';
+    EXECUTE IMMEDIATE opSql USING purl, category_name, or_category_name;
     COMMIT;
 END;
 /
--- src/procedimientos/proc_updatetags.sql
+++ src/procedimientos/proc_updatetags.sql
@@ -1,12 +1,13 @@
 CREATE OR REPLACE PROCEDURE update_tag(
     or_tag_name tags.tag_name%TYPE,
-    tag_name tags.tag_name%TYPE
+    tag_name tags.tag_name%TYPE,
+    purl_tag tags.url_tag%TYPE
 )
 IS
     opSql VARCHAR(255);
 BEGIN
-    opSql := 'UPDATE tags SET tag_name = :tag_name WHERE tag_name = :or_tag_name';
-    EXECUTE IMMEDIATE opSql USING tag_name, or_tag_name;
+    opSql := 'UPDATE tags SET url_tag = :purl_tag, tag_name = :tag_name WHERE tag_name = :or_tag_name';
+    EXECUTE IMMEDIATE opSql USING purl_tag, tag_name, or_tag_name;
     COMMIT;
 END;
 /
